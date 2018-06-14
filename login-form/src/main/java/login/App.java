46da5e282835c28693ae37ff0448c6328ade7647
Bart <bartpetka@gmail.com>
2018-06-14 17:51:25 +0200

    Implement static handler

--- login-form/src/main/java/login/App.java
+++ login-form/src/main/java/login/App.java
@@ -1,13 +1,17 @@
 package login;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(7800), 0);
+        server.createContext("/login", new LoginHandler());
+        server.createContext("/static", new StaticHandler());
+        server.setExecutor(null);
+        server.start();
     }
 }
